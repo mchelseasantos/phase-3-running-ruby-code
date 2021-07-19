cd8b86fa6f911222f5e395d783078a69a95977ed
chelseasantos <mchelseasantos@gmail.com>
2021-07-18 22:40:42 -0600

    Completed Assignment

--- app.rb
+++ app.rb
@@ -7,10 +7,10 @@
 
 #pp [{id: 1, hello: "World"}, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}, {id: 4, hello: "Learner"}]
 
-it 'outputs the string "Pass this test, please." using the print method' do
-    expect { load 'app.rb' }.to output(a_string_including("Pass this test, please.")).to_stdout
-  end 
+# it 'outputs the string "Pass this test, please." using the print method' do
+#     expect { load 'app.rb' }.to output(a_string_including("Pass this test, please.")).to_stdout
+#   end 
 
 puts "Hello World!"
-print "Pass this test, please"
+print "Pass this test, please."
 p [1, 2, 3]
